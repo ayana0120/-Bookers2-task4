36ff5c30e5f98d281316da78bb8ee78e8ad680a0
ayana0120 <keichan0501.ayana@gmail.com>
2020-09-05 17:09:01 +0900

    [Add]フォロー機能追加

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { minimum: 2, maximum: 20}
+  validates :introduction, length: { maximum: 50 }
+
+  has_many :books, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  has_many :followings, through: :active_relationships, source: :follower
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :passive_relationships, source: :followings
+  attachment :profile_image
+
+  def followed_by?(user)
+    passive_relationships.where(following_id: user.id).exists?
+  end
+
+end
